6c937ee4a0f0cdf6f8182fbfe226ef1d7375b369
Dmitry Lebedev <p17dmitry@gmail.com>
2016-07-30 13:05:22 +0000

    Base version

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+var strftime = require('strftime')
+var path = require('path')
 var express = require('express')
 var app = express()
 
 app.get('/', function(req, res) {
-    res.send('Hello New World!')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.listen(8080, function() {
-    console.log('Express app listening on port 8080')
+app.get('/:timestamp', function(req, res) {
+    var response = { unix: null, natural: null }
+    var timestamp = req.params.timestamp
+    var date
+    if (isNaN(timestamp)) {
+        date = new Date(timestamp)
+    } else {
+        date = new Date(+timestamp * 1000)
+    }
+    if (!isNaN(date.getTime())) {
+        response = {
+            unix: Math.floor(date.getTime() / 1000),
+            natural: strftime('%B %e, %Y', date)
+        }
+    }
+    res.json(response)
 })
+
+app.listen(process.env.port || 8080)
